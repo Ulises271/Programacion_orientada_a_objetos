d5b123c1cafde06a822d40ab047655e87cd150ca
Ulises Muñoz <munozcanoadolfoulises@gmail.com>
2023-02-22 21:22:59 -0600

    Add existing file

---
+++ Tarea3_gato.java
@@ -0,0 +1,12 @@
+public class Tarea3_gato {
+    public static void main(String[] args) {
+        System.out.println("   - - - - - ");
+        System.out.println(" │           │");
+        System.out.println(" ");
+        System.out.println(" │           │");
+        System.out.println(" ");
+        System.out.println(" │           │");
+        System.out.println("   - - - - - ");
+    }
+
+}
